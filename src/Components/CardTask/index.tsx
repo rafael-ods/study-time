2843c1cca8e45c71efb9d800b5387f33cbfe0463
rafael-ods <rafaeloliveirafinanca@gmail.com>
2023-05-07 17:30:05 -0300

    feat: add icone delete and changes in styles of icone finished

--- src/Components/CardTask/index.tsx
+++ src/Components/CardTask/index.tsx
@@ -1,7 +1,7 @@
 import { ICardTask } from "../../Shared/ICardTask";
 import style from "./style.module.scss";
 import { BsJournalCheck } from "react-icons/bs";
-
+import { RiDeleteBin2Line } from "react-icons/ri";
 interface CardProps {
   cards: ICardTask[];
   selectedTask: (taskSelected: ICardTask) => void;
@@ -16,26 +16,30 @@ const CardTask = ({ cards, selectedTask }: CardProps) => {
           <li
             className={`${style.card__item} ${
               card.selected ? style.itemActive : ""
-            } ${card.completed ? style.backgroundComplete : ""}`}
+            }`}
             key={card.id}
             onClick={() => {
-                !card.completed && selectedTask({
-                id: card.id,
-                task: card.task,
-                time: card.time,
-                selected: false,
-                completed: false,
-              });
+              !card.completed &&
+                selectedTask({
+                  id: card.id,
+                  task: card.task,
+                  time: card.time,
+                  selected: false,
+                  completed: false,
+                });
             }}
           >
             <div>
               <h3 className={style.card__title}>{card.task}</h3>
               <span className={style.card__span}>{card.time}</span>
             </div>
-            <BsJournalCheck
-              aria-label="Tarefa Completada"
-              className={`${card.completed ? style.showIcon : style.icon}`}
-            />
+            <div className={style.card__boxIcon}>
+              <BsJournalCheck
+                aria-label="Tarefa Completada"
+                className={`${card.completed ? style.showIcon : style.icon}`}
+              />
+              <RiDeleteBin2Line color="red" size={28} />
+            </div>
           </li>
         );
       })}
